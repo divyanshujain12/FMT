b96380d2042031a4610f2016d9e099f68cd70d35
divyanshu.jain <divyanshujain12@hotmail.com>
2019-06-19 16:24:02 +0530

    first commit

---
+++ app/src/main/java/com/application/fmt/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.application.fmt.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.application.fmt.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/application/fmt/globalClasses/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.application.fmt.globalClasses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+
+}
