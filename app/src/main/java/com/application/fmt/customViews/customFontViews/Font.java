38604bd572c331ba7e70aa6d96f0f58c473f7ae5
divyanshujain1312 <archana.gupta@jktech.com>
2019-06-20 15:28:14 +0530

    adding ui

---
+++ app/src/main/java/com/application/fmt/customViews/customFontViews/Font.java
@@ -0,0 +1,11 @@
+package com.application.fmt.customViews.customFontViews;
+
+/**
+ * Created by divyanshu.jain on 8/17/2017.
+ */
+
+public interface Font {
+
+    String FONT_REGULAR = "fonts/Titillium-Regular.otf";
+    String FONT_BOLD = "fonts/Titillium-Bold.otf";
+}
