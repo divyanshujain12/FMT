5ffc80f13834632f5eab92d77c6235fdddb71c59
divyanshu.jain <divyanshujain12@hotmail.com>
2019-07-17 12:46:51 +0530

    adding header

--- app/src/main/java/com/application/fmt/ApiUtils/GetDataService.java
+++ app/src/main/java/com/application/fmt/ApiUtils/GetDataService.java
@@ -10,30 +10,38 @@ import retrofit2.http.POST;
 import rx.Observable;
 
 public interface GetDataService {
-    @Headers("Content-Type: application/json")
+    @Headers({"Content-Type: application/json",
+            "device-type:android"})
     @POST("users/email_exists")
     Observable<JsonElement> checkEmailExist(@Body JsonObject requestJson);
 
-    @Headers("Content-Type: application/json")
+    @Headers({"Content-Type: application/json",
+            "device-type:android"})
     @POST("users/mobile_exists")
     Observable<JsonElement> checkMobileExist(@Body JsonObject requestJson);
 
+    @Headers({"Content-Type: application/json",
+            "device-type:android"})
     @GET("users/country_list")
     Observable<JsonElement> getCountries();
 
-    @Headers("Content-Type: application/json")
+    @Headers({"Content-Type: application/json",
+            "device-type:android"})
     @POST("users/otp")
     Observable<JsonElement> sendOtp(@Body JsonObject requestJson);
 
-    @Headers("Content-Type: application/json")
+    @Headers({"Content-Type: application/json",
+            "device-type:android"})
     @POST("users")
     Observable<JsonElement> register(@Body JsonObject requestJson);
 
-    @Headers("Content-Type: application/json")
+    @Headers({"Content-Type: application/json",
+            "device-type:android"})
     @POST("users/sign_in")
     Observable<JsonElement> login(@Body JsonObject requestJson);
 
-    @Headers("Content-Type: application/json")
+    @Headers({"Content-Type: application/json",
+            "device-type:android"})
     @POST("users/login_otp")
     Observable<JsonElement> loginOtp(@Body JsonObject requestJson);
 }
